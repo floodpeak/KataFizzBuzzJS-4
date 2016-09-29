74dfee35e445f432bc2b4251ae2584a879b48b02
floodpeak <hongfengzhou@yahoo.com>
2016-09-30 01:49:19 +0800

    refactor the code of both base

--- getMaskedNumber.js
+++ getMaskedNumber.js
@@ -4,14 +4,11 @@ module.exports = function(number){
   var isMultiple3 = number % base1 == 0
   var isMultiple5 = number % base2 == 0
   var result = ''
-  if(isMultiple3 && isMultiple5){
-    result = 'FizzBuzz'
-  }
   if(isMultiple3){
-    result = 'Fizz'
+    result += 'Fizz'
   }
   if(isMultiple5){
-    result = 'Buzz'
+    result += 'Buzz'
   }
   return result == '' ? number : result
 }
