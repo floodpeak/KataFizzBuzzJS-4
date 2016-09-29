cec8135170abe58bb4bc5e91f235cbf16a705099
floodpeak <hongfengzhou@yahoo.com>
2016-09-30 01:38:14 +0800

    create base 1 as 3

--- getMaskedNumber.js
+++ getMaskedNumber.js
@@ -1,8 +1,11 @@
+var base1 = 3
+
 module.exports = function(number){
-  if(number % 3 == 0 && number % 5 == 0){
+
+  if(number % base1 == 0 && number % 5 == 0){
     return 'FizzBuzz'
   }
-  if(number % 3 == 0){
+  if(number % base1 == 0){
     return 'Fizz'
   }
   if(number % 5 == 0){
