0621e8416a0714f519c1f649691f4eddd0370ef6
floodpeak <hongfengzhou@yahoo.com>
2016-09-30 01:44:36 +0800

    refactor the condition of mutiple 3 and 5

--- getMaskedNumber.js
+++ getMaskedNumber.js
@@ -1,14 +1,15 @@
 var base1 = 3
 var base2 = 5
 module.exports = function(number){
-
-  if(number % base1 == 0 && number % base2 == 0){
+  var isMultiple3 = number % base1 == 0
+  var isMultiple5 = number % base2 == 0
+  if(isMultiple3 && isMultiple5){
     return 'FizzBuzz'
   }
-  if(number % base1 == 0){
+  if(isMultiple3){
     return 'Fizz'
   }
-  if(number % base2 == 0){
+  if(isMultiple5){
     return 'Buzz'
   }
   return number
